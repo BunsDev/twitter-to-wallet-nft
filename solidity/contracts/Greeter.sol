0508b7e60d7e559d742a33c3efecb53cbad0e4f8
St4rgarden <stargardenbeats@gmail.com>
2023-03-03 15:47:18 -0800

    scaffolding 2

--- solidity/contracts/Greeter.sol
+++ solidity/contracts/Greeter.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.4 <0.9.0;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "../deps/ERC721.sol";
-
+import "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 
 contract JailBreaker is ERC721 {
 
